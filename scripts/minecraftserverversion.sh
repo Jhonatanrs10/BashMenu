2229a73807d80cf91b6afa6a42575eaadf92766e
Jhonatanrs <102379510+Jhonatanrs10@users.noreply.github.com>
2023-06-01 21:18:49 -0300

    save 2

--- scripts/minecraftserverversion.sh
+++ scripts/minecraftserverversion.sh
@@ -1,66 +1,20 @@
 #!/usr/bin/env sh
 installMinecraftServerVersion(){
-	nomeMSV="Teste"
 	echo "NOME DO SERVIDOR/PASTA/BIN (SERA USADO NOS 3)"
 	read nomeMSV
-	#Forges
-	echo "=====================
-Servers Links:
-=====================
-FORGE 1.16.5
-https://maven.minecraftforge.net/net/minecraftforge/forge/1.16.5-36.2.34/forge-1.16.5-36.2.34-installer.jar
 
-FORGE 1.7.10
-https://maven.minecraftforge.net/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10/forge-1.7.10-10.13.4.1614-1.7.10-installer.jar
-
-MOHISTMC 1.16.5
-https://mohistmc.com/builds/1.16.5/mohist-1.16.5-1107-server.jar
-"
-	echo "COLE O LINK DE DOWNLOAD DO SERVER"
-	read varServer
-	#Mods
-	echo "=====================
-Mods Links:
-=====================
-
-LuckyBlock 1.7.10
-https://drive.google.com/u/0/uc?id=16yDUK00znMvOsLPEg6XBZbKCPTjK05ir&export=download
-
-PixelMon 1.16.5
-https://mediafilez.forgecdn.net/files/4110/761/Pixelmon-1.16.5-9.1.0-universal.jar
-"	
-	echo "COLE O LINK DE DOWNLOAD DO MOD"
-	read varMods
-	#Plugins
-	echo "=====================
-Plugins Links:
-=====================
-
-WORLD EDIT 1.16.5
-https://mediafilez.forgecdn.net/files/4162/208/worldedit-mod-7.2.13.jar
-
-VAULT 1.16.5
-https://www.spigotmc.org/resources/vault.34315/download?version=344916
-
-SKYWARS RELOADED 1.16.5
-https://www.spigotmc.org/resources/skywarsreloaded.3796/download?version=263166
-
-
-"	
-	echo "COLE O LINK DE DOWNLOAD DO PLUGIN"
-	read varPlugins
+	yesorno "MINECRAFT SERVER FORGE 1.7.10" "setLink https://maven.minecraftforge.net/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10/forge-1.7.10-10.13.4.1614-1.7.10-installer.jar"
+	yesorno "MINECRAFT SERVER FORGE 1.16.5" "setLink https://maven.minecraftforge.net/net/minecraftforge/forge/1.16.5-36.2.34/forge-1.16.5-36.2.34-installer.jar"
+	yesorno "MINECRAFT SERVER MOHIST 1.16.5" "setLink https://mohistmc.com/builds/1.16.5/mohist-1.16.5-1107-server.jar"
+	yesorno "MINECRAFT SERVER FORGE 1.18.2" "setLink https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.2.0/forge-1.18.2-40.2.0-installer.jar"
 
 	echo -e "[INFO] - CRIANDO DIRETORIOS... - [INFO]"
 	criaDiretorio "diretorioServer" "$HOME/.Jhonatanrs/MinecraftServer-$nomeMSV"
-    criaDiretorio "diretorioMods" "$HOME/.Jhonatanrs/MinecraftServer-$nomeMSV/mods"
-	criaDiretorio "diretorioPlugins" "$HOME/.Jhonatanrs/MinecraftServer-$nomeMSV/plugins"
 
 	echo -e "[INFO] - BAIXANDO ARQUIVOS... - [INFO]"
-	criaPastaBaixaExtrai "$diretorioServer/" "$varServer" "server.jar"
-	criaPastaBaixaExtrai "$diretorioMods/" "$varMods" "mod.jar"
-	criaPastaBaixaExtrai "$diretorioPlugins/" "$varPlugins" "plugin.jar"
+	criaPastaBaixaExtrai "$diretorioServer/" "$varLink" "server.jar"
 
-	echo -e "[INFO] - INSTALANDO MINECRAFT SERVER - [INFO]"
+	echo -e "[INFO] - INSTALANDO - [INFO]"
 	cd $diretorioServer
 	java -jar server.jar --installServer
 	
