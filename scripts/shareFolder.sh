70edff9493f84a584471be11e024350ed1fecd72
Jhonatanrs10 <jhonatanrs10@hotmail.com>
2023-07-31 21:44:26 -0300

    add comando systemctl enable e start ao pacote smb

--- scripts/shareFolder.sh
+++ scripts/shareFolder.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 criaPastaShareUser(){
 	installPacotes "samba"
+        sudo systemctl enable smb.service
+        sudo systemctl start smb.service --now
         criaSeuUsuarioSamba
         criaDiretorioShare "$HOME/Share" "Share"
 }
