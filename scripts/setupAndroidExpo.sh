3e97e56328c191c85654cae6ad10be419df4d00e
jhonatanr10 <jhonatanrs10@hotmail.com>
2023-09-19 13:18:40 -0300

    corrigindo zerotier e setup expo

--- scripts/setupAndroidExpo.sh
+++ scripts/setupAndroidExpo.sh
@@ -2,7 +2,13 @@
 
 setupAndroidExpo(){
     installPacotes "nodejs npm yarn"
-    yesorno "INSTALAR EXPO-CLI (NPM)" "sudo npm install expo-cli --global"
+    menu12345 "
+[1] INSTALAR EXPO-CLI YARN
+[2] INSTALAR EXPO-CLI NPM
+" "sudo yarn add expo-cli --global
+" "sudo npm install expo-cli --global"
+
+    yesorno "UPGRADE EXPO-CLI" "expo upgrade"
 
     criaDiretorioInstall "$HOME/AndroidExpo"
     echo "INICIAR UM PROJETO
@@ -10,7 +16,7 @@ expo init NOME-PROJETO
 ENTRAR NA PASTA DO PROJETO
 cd NOME-PROJETO
 INICIAR O PROJETO
-expo start "
+expo start"
 }
 
 criarProjetoExpo(){
--- scripts/zerotier.sh
+++ scripts/zerotier.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env sh
 zerotier(){
-    sudo dpkg --configure -a
-    curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
-    if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
-    sudo zerotier-cli join d5e5fb6537d99ad3
-    read entrar
+    echo "Zerotier para Ubuntu[1] ou Arch[2]"
+    read escolha
+    if [ $escolha = "1" ]; then
+        sudo dpkg --configure -a
+        curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
+        if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
+        enableSystemctl "zerotier-one"
+        sudo zerotier-cli join d5e5fb6537d99ad3
+    elif [ $escolha = "2" ]; then
+        echo "PAMAC ou PACMAN"
+        installPacotes "zerotier-one"
+        enableSystemctl "zerotier-one"
+        sudo zerotier-cli join d5e5fb6537d99ad3
+    fi 
+   
 }
