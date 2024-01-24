b5a4651d71aba78bef98db3f2608c2848753f4a8
jhonatanrs10 <jhonatanrs10@hotmail.com>
2024-01-24 10:25:40 -0300

    att na roça

--- scripts/posInstall.sh
+++ scripts/posInstall.sh
@@ -23,24 +23,23 @@ posInstall(){
                 clear
                 echo "[PosInstall]
 [1]Configurar I3WM
-[2]Configurar Bluetooth
-[3]Configurar Network [ARCH]
-[4]Configurar Funcionalidades
-[5]Configurar NVIDIA [ARCH]
-[6]Configuracoes Manuais
-[7]Configurar Touchpad
-[8]Fix Bluetooth & Network"
+[2]Configurar Pos-Install [ARCH]
+[3]Configurar NVIDIA [ARCH]
+[4]Configurar Teclado PT-BT
+[5]Configurar Touchpad
+[6]Configurar Funcionalidades
+[7]Configuracoes Manuais
+[8]Configurar Network
+[9]Fix Bluetooth & Network Interf."
                 read resp
                 if [ "$resp" = "1" ]; then
                         i3wmConfig
                 elif [ "$resp" = "2" ]; then
-                        echo "[ARCH] Bluetooth"
-                        #sudo systemctl enable bluetooth.service
-                        #sudo systemctl start bluetooth.service --now
+                        echo "[ARCH] Pos-Install"
+                        installPacotes "i3 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pulseaudio samba xarchiver papirus-icon-theme breeze-gtk xcursor-comix ntfs-3g dosfstools os-prober chromium nano git neofetch gufw gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer ffmpeg fwupd samba gvfs-smb flatpak gvfs gvfs-mtp gvfs-smb udisks2 net-tools bluez bluez-tools bluez-utils noto-fonts-emoji man-db font-manager"
+                        enableSystemctl "lightdm"
                         enableSystemctl "bluetooth"
-                        echo "INSTALAR blueberry"
-                        installPacotes "blueberry"
-                elif [ "$resp" = "3" ]; then
+                elif [ "$resp" = "8" ]; then
                         echo "[ARCH] Network"
                         echo "INSTALAR NETWORKMANAGER"
                         installPacotes "networkmanager nm-connection-editor network-manager-applet"
@@ -49,18 +48,20 @@ posInstall(){
                         enableSystemctl "NetworkManager"
                         #echo "REMOVER IWD (wifi terminal archinstall)"
                         #removePacotes "iwd"
-                elif [ "$resp" = "4" ]; then
+                elif [ "$resp" = "6" ]; then
                         clear
                         echo "##################################"
                         echo "Funcionalidades"
                         echo "##################################"
-                        echo "Aplicativos"
+                        echo "[ARCH] Aplicativos"
                         installPacotes "nano git neofetch"
-                        echo "FireWall"
+                        installPacotes "gimp inkscape qbittorrent"
+                        installPacotes "retroarch retroarch-assets-xmb retroarch-assets-ozone libretro-snes9x"
+                        echo "[ARCH] FireWall"
                         installPacotes "gufw"
                         echo "[ARCH] Plugins Multimedia"
                         installPacotes "gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer ffmpeg fwupd"
-                        echo "Samba para pastas na rede"
+                        echo "[ARCH] Rede Share"
                         installPacotes "samba gvfs-smb"
                         echo "[ARCH] Suporte a Flatpak"
                         installPacotes "flatpak"
@@ -74,13 +75,13 @@ posInstall(){
                         installPacotes "udisks2"
                         echo "[ARCH] IFCONFIG"
                         installPacotes "net-tools"
-                elif [ "$resp" = "5" ]; then
+                elif [ "$resp" = "3" ]; then
                         echo "[ARCH] NVIDIA"
                         #https://github.com/lutris/docs/blob/master/InstallingDrivers.md#arch--manjaro--other-arch-linux-derivatives
                         #causa crash no gdm o pacote: nvidia-dkms
                         #https://codigocristo.github.io/driver_nvidia.html
                         installPacotes "nvidia nvidia-settings nvidia-utils lib32-nvidia-utils cuda opencl-nvidia lib32-opencl-nvidia vdpauinfo clinfo"   
-                elif [ "$resp" = "6" ]; then
+                elif [ "$resp" = "7" ]; then
                         echo "---------------------
 Configuracoes Manuais
 ---------------------
@@ -99,10 +100,9 @@ Apos editar, executar:
 sudo systemctl restart gdm3
 "
                         read enterprasair
-                elif [ "$resp" = "7" ]; then
+                elif [ "$resp" = "5" ]; then
                         i3touchpad
-
-                elif [ "$resp" = "8" ]; then
+                elif [ "$resp" = "9" ]; then
                         echo "[1]Fix Bluetooth & Network Interference [2]Remove Fix"
                         read esco
                         if [ "$esco" = "1" ]; then
@@ -110,9 +110,10 @@ sudo systemctl restart gdm3
                         elif [ "$esco" = "2" ]; then
                                 sudo rm /etc/modprobe.d/iwlwifi-opt.conf
                         fi
-                elif [ "$resp" = "9" ]; then
-                        echo "[MANJARO] Apps most used (Flatpak)"
-                        installPacotes "org.chromium.Chromium com.visualstudio.code-oss com.bitwarden.desktop com.obsproject.Studio com.valvesoftware.Steam com.google.AndroidStudio com.heroicgameslauncher.hgl com.discordapp.Discord org.gimp.GIMP org.inkscape.Inkscape org.shotcut.Shotcut org.nickvision.tubeconverter io.github.jeffshee.Hidamari"
+                elif [ "$resp" = "4" ]; then
+                        echo "[ARCH] Keyboard BR"
+                        setxkbmap -layout br
+                        echo "setxkbmap -layout br" >> ~/.profile
                 else
                         echo "OPCAO NAO ENCONTRADA!"
                         sleep 1
@@ -139,21 +140,18 @@ EOF
 #Yay e Paru para gerenciar AUR
 
 i3wmConfig(){
-        installPacotes "dmenu rofi i3lock i3status feh nitrogen htop light volumeicon pcmanfm scrot terminology lxrandr lxappearance xfce4-power-manager speedcrunch system-config-printer bluez bluez-tools blueman network-manager-applet pavucontrol"
+        #MAIN COLOR 005577, bfbfbf
+        installPacotes "dmenu rofi i3lock i3status feh nitrogen htop cmatrix acpilight volumeicon pcmanfm scrot xsel terminology lxrandr lxappearance xfce4-taskmanager xfce4-power-manager galculator system-config-printer blueman network-manager-applet pavucontrol"
         cp $HOME/.config/i3/config $HOME/.config/i3/config-bkp
-        light -N 0.06
-        echo "Cor do Tema: Ex[DMENU:005577] ,[GNOME:4a86cf]"
-        read corBase
-
-        if [ "$corBase" = "" ]; then
-               corBase=005577
-               echo "Valor vazio, Default $corBase"
-        elif [ "`expr length $corBase`" = "6" ]; then
-               echo "Escolha $corBase"
-        else
-                corBase=005577
-                echo "Necessario 6 digitos, Default $corBase"    
-        fi
+        forLength "[COLOR] Bar" "7" "#000000"
+        jrsbar=$txtForLength
+        jrswindowtextosemfoco=$jrsbar
+        forLength "[COLOR] Text" "7" "#ffffff"
+        jrsbartexto=$txtForLength
+        jrswindowtextocomfoco=$jrsbartexto
+        forLength "[COLOR] Window" "7" "#005577"
+        jrswindowcomfoco=$txtForLength
+        jrswindowsemfoco="#7d7d7d"
         criarArq '######Jhonatanrs I3-WM config######
 ###VARs###
 set $mod Mod4
@@ -162,14 +160,16 @@ set $wallpaper $HOME/.config/i3/Wallpaper.png
 #set $appMenu dmenu_run
 set $appMenu rofi -combi-modi drun#ssh#combi -show combi -window-title Rofi -scroll-method 1 -show-icons -combi-display-format "{text} ({mode})" -config $HOME/.config/i3/rofi.rasi 
 set $appTerminal terminology
-set $appF1 pcmanfm
-set $appF2 nitrogen
-set $appF3 lxrandr
-set $appF4 lxappearance
-set $appF5 xfce4-power-manager-settings
-set $appF6 speedcrunch
-set $appF7 system-config-printer
-set $appF8 pavucontrol
+set $appFiles pcmanfm
+set $appBrowser chromium
+set $appF1 pavucontrol
+set $appF2 galculator
+set $appF3 xfce4-taskmanager
+set $appF4 xfce4-power-manager-settings
+set $appF5 lxrandr
+set $appF6 lxappearance
+set $appF7 nitrogen
+set $appF8 system-config-printer
 set $refresh_i3status killall -SIGUSR1 i3status
 set $Locker i3lock -c 000000 && sleep 1
 set $ws1 "1"
@@ -182,20 +182,12 @@ set $ws7 "7"
 set $ws8 "8"
 set $ws9 "9"
 set $ws10 "10"
-set $corcomfoco "#'$corBase'"
-set $textocomfoco "#ffffff"
-set $corsemfoco "#bfbfbf"
-set $textosemfoco "#000000"
-set $black "#000000"
-set $white "#ffffff"
-set $corbar "#000000"
-set $cortexto "#ffffff" #alguns itens a cor esta no i3status
-set $indcomfoco "#009c66"
-set $indsemfoco "#a6ffa4"
 set $tamanhodasbordas 3px
 set $espacoentrejanelas 5px
+
 ###Font###
 font pango:$textFont
+
 ###AutoStart###
 exec --no-startup-id dex --autostart --environment i3
 exec --no-startup-id xss-lock --transfer-sleep-lock -- i3lock --nofork
@@ -205,15 +197,21 @@ exec --no-startup-id blueman-applet
 exec --no-startup-id xfce4-power-manager
 exec --no-startup-id nitrogen --restore
 #exec --no-startup-id feh --bg-scale $wallpaper
+
 ###Binds###
 bindsym $mod+d exec --no-startup-id $appMenu
 bindsym $mod+Return exec $appTerminal
 bindsym $mod+Shift+Return exec i3-sensible-terminal
 bindsym $mod+Shift+q kill
-#print
-bindsym --release Print exec scrot Screenshot_%Y-%m-%d_%H-%M-%S.png
-bindsym --release $mod+Print exec scrot -s Cutshot_%Y-%m-%d_%H-%M-%S.png
-#edit window
+bindsym $mod+x exec $appFiles
+bindsym $mod+c exec $appBrowser
+
+###Print###
+bindsym --release Print exec mkdir -p ~/PrtSc | scrot ~/PrtSc/creenshot_%Y-%m-%d_%H-%M-%S.png
+bindsym --release $mod+Print exec mkdir -p ~/PrtSc | scrot -s ~/PrtSc/Cutshot_%Y-%m-%d_%H-%M-%S.png
+bindsym --release $mod+z exec $HOME/.config/i3/getcol.sh
+
+###Window###
 bindsym $mod+h focus left
 bindsym $mod+j focus down
 bindsym $mod+k focus up
@@ -230,8 +228,7 @@ bindsym $mod+Shift+Left move left
 bindsym $mod+Shift+Down move down
 bindsym $mod+Shift+Up move up
 bindsym $mod+Shift+Right move right
-bindsym $mod+b split h
-bindsym $mod+v split v
+bindsym $mod+v split toggle
 bindsym $mod+f fullscreen toggle
 bindsym $mod+s layout stacking
 bindsym $mod+w layout tabbed
@@ -240,14 +237,18 @@ bindsym $mod+Shift+space floating toggle
 bindsym $mod+space focus mode_toggle
 bindsym $mod+a focus parent
 bindsym $mod+Shift+w sticky toggle
-#brightness and audio
+
+###Brightness and Sound###
 bindsym XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +5% && $refresh_i3status
 bindsym XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -5% && $refresh_i3status
 bindsym XF86AudioMute exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status
 bindsym XF86AudioMicMute exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle && $refresh_i3status
-bindsym XF86MonBrightnessUp exec --no-startup-id light -A 5 && echo `light` > $HOME/.config/i3/brightness &&  $refresh_i3status
-bindsym XF86MonBrightnessDown exec --no-startup-id light -U 5 && echo `light` > $HOME/.config/i3/brightness  &&  $refresh_i3status
-#select workspaces and move contents
+#bindsym XF86MonBrightnessUp exec --no-startup-id light -A 5 && echo `light` > $HOME/.config/i3/brightness &&  $refresh_i3status
+#bindsym XF86MonBrightnessDown exec --no-startup-id light -U 5 && echo `light` > $HOME/.config/i3/brightness  &&  $refresh_i3status
+bindsym XF86MonBrightnessUp exec --no-startup-id xbacklight -dec 5 &&  $refresh_i3status
+bindsym XF86MonBrightnessDown exec --no-startup-id xbacklight -inc 5 &&  $refresh_i3status
+
+###Workspaces###
 bindsym $mod+1 workspace number $ws1
 bindsym $mod+2 workspace number $ws2
 bindsym $mod+3 workspace number $ws3
@@ -268,21 +269,24 @@ bindsym $mod+Shift+7 move container to workspace number $ws7
 bindsym $mod+Shift+8 move container to workspace number $ws8
 bindsym $mod+Shift+9 move container to workspace number $ws9
 bindsym $mod+Shift+0 move container to workspace number $ws10
-#reset file config
 bindsym $mod+Shift+c reload
 bindsym $mod+Shift+r restart
-###i3Configs###
 floating_modifier $mod
+
+###i3Configs###
 tiling_drag modifier titlebar
 title_align center
 hide_edge_borders none
 default_border pixel $tamanhodasbordas
+for_window [all] title_window_icon padding 5px
 default_floating_border pixel $tamanhodasbordas
 gaps inner $espacoentrejanelas
 gaps outer 0px
 smart_gaps off
 workspace_layout default
 for_window [class=$appTerminal] floating desable
+for_window [title=$appF2] floating enable
+
 ###I3BARS###
 bar {
 	status_command i3status --config ~/.config/i3/i3status.conf
@@ -300,25 +304,26 @@ bar {
 	padding 0px
 	colors {
 	    #i3bar
-	    background $corbar
-	    statusline $cortexto
-	    separator $cortexto
-	    focused_workspace  $corcomfoco $corcomfoco $cortexto
-	    active_workspace   $corbar $corbar $cortexto
-	    inactive_workspace $corbar $corbar $cortexto
+	    background '$jrsbar'
+	    statusline '$jrsbartexto'
+	    separator '$jrsbartexto'
+	    focused_workspace  '$jrswindowcomfoco' '$jrswindowcomfoco' '$jrsbartexto'
+	    active_workspace   '$jrsbar' '$jrsbar' '$jrsbartexto'
+	    inactive_workspace '$jrsbar' '$jrsbar' '$jrsbartexto'
 	    urgent_workspace   #2f343a #900000 #ffffff
        	    binding_mode       #2f343a #900000 #ffffff
     }
 }
 #class                  borda       background  texto         indicator   child_border
-client.focused          $corcomfoco $corcomfoco $textocomfoco $corcomfoco $corcomfoco
-client.focused_inactive $corsemfoco $corsemfoco $textosemfoco $corsemfoco $corsemfoco
-client.unfocused        $corsemfoco $corsemfoco $textosemfoco $corsemfoco $corsemfoco
+client.focused          '$jrswindowcomfoco' '$jrswindowcomfoco' '$jrswindowtextocomfoco' '$jrswindowcomfoco' '$jrswindowcomfoco'
+client.focused_inactive '$jrswindowsemfoco' '$jrswindowsemfoco' '$jrswindowtextosemfoco' '$jrswindowsemfoco' '$jrswindowsemfoco'
+client.unfocused        '$jrswindowsemfoco' '$jrswindowsemfoco' '$jrswindowtextosemfoco' '$jrswindowsemfoco' '$jrswindowsemfoco'
 client.urgent           #2f343a #900000 #ffffff #900000 #900000
-client.placeholder      #000000 #0c0c0c #ffffffff #000000 #0c0c0c
+client.placeholder      #000000 #0c0c0c #ffffff #000000 #0c0c0c
 client.background       #ffffff
+
 ###MODOS###
-set $mode_programs (1)Files, (2)Wallpapers, (3)Display, (4)Appearance, (5)PowerManager (6)Calculator (7)Printer (8)Sound
+set $mode_programs (1)Sound, (2)Calculator, (3)Tasks, (4)PowerManager, (5)Display (6)Appearance (7)Wallpaper (8)Printer
 mode "$mode_programs" {
     bindsym 1 exec $appF1, mode "default"
     bindsym 2 exec $appF2, mode "default"
@@ -338,8 +343,10 @@ mode "$mode_sound" {
     bindsym Up exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +5% && $refresh_i3status
     bindsym Down exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -5% && $refresh_i3status
     bindsym m exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status
-    bindsym Right exec --no-startup-id light -A 5 && echo `light` > $HOME/.config/i3/brightness &&  $refresh_i3status
-    bindsym Left exec --no-startup-id light -U 5 && echo `light` > $HOME/.config/i3/brightness  &&  $refresh_i3status
+    #bindsym Right exec --no-startup-id light -A 5 && echo `light` > $HOME/.config/i3/brightness &&  $refresh_i3status
+    #bindsym Left exec --no-startup-id light -U 5 && echo `light` > $HOME/.config/i3/brightness  &&  $refresh_i3status
+    bindsym Right exec --no-startup-id xbacklight -dec 5 &&  $refresh_i3status
+    bindsym Left exec --no-startup-id xbacklight -inc 5 &&  $refresh_i3status
     bindsym Return mode "default"
     bindsym Escape mode "default"
 }
@@ -372,11 +379,6 @@ mode "$mode_resize" {
         bindsym Escape mode "default"
 }
 bindsym $mod+r mode "$mode_resize"
-
-###AVISOS###
-#TECLADO CONFIG
-#setxkbmap -query
-#setxkbmap -model pc105 -layout br,us -option grp:alt_caps_toggle
 ' "$HOME/.config/i3/config-jrsbkp.conf"
 #
 #
@@ -391,7 +393,7 @@ criarArq '# i3status configuration file.
 general {
 	colors = true
         interval = 5
-	color_good = "#FFFFFF"
+	color_good = "'$jrsbartexto'"
 	color_bad = "#FF0000"
 	color_degraded = "#FFFF00"
 }
@@ -410,7 +412,8 @@ order += "tztime local"
 
 read_file BRIGHTNESS {
 	format = "[☼/%content]"
-	path = "'$HOME'/.config/i3/brightness"
+	#path = "'$HOME'/.config/i3/brightness"
+        path = "/sys/class/backlight/intel_backlight/brightness"
 	max_characters = 5
 	separator = false
 	separator_block_width = 1
@@ -522,14 +525,12 @@ tztime local {
 criarArq '
 * {
 
-    corcomfoco: #'$corBase';
-    textocomfoco: #ffffff;
-    corsemfoco: #bfbfbf;
-    textosemfoco: #000000;
-    black: #000000;
-    white: #ffffff;
-    corbar: #000000;
-    cortexto: #ffffff;
+    corcomfoco: '$jrswindowcomfoco';
+    textocomfoco: '$jrswindowtextocomfoco';
+    corsemfoco: '$jrswindowsemfoco';
+    textosemfoco: '$jrswindowtextosemfoco';
+    corbar: '$jrsbar';
+    cortexto: '$jrsbartexto';
   
 }
 
@@ -624,11 +625,11 @@ element.alternate.active {
     text-color:       @corcomfoco;
 }
 scrollbar {
-    background-color: @white;
+    background-color: @cortexto;
     handle-color: @corsemfoco;
     width:        4px ;
     border:       0;
-    handle-width: 5px;
+    handle-width: 3px;
     padding:      0;
 }
 mode-switcher {
@@ -657,7 +658,7 @@ prompt {
     text-color: @textocomfoco;
 }
 inputbar {
-    border-color: @white;
+    border-color: @cortexto;
     border: 0px 0px 1px 0px;
     margin: 5px 10px 10px 10px;
     padding: 5px 0px 5px 0px;
@@ -672,10 +673,24 @@ textbox-prompt-colon {
 
 ' "$HOME/.config/i3/rofi.rasi"
 
-        sudo chmod +s /usr/bin/light
+criarArq '#!/bin/sh
+getcol=$(rm /tmp/getcol.png
+        scrot -s /tmp/getcol.png
+        convert /tmp/getcol.png \
+	-define histogram:unique-colors=true \
+	-format %c histogram:info:- | \
+	sort -nr | \
+	sed -n '"'""1s/[^#]*\([^ ]*\).*/\1/p""'"')
+echo $getcol | xsel -bi
+' "$HOME/.config/i3/getcol.sh"
+
+        sudo chmod 777 $HOME/.config/i3/getcol.sh
+        #sudo chmod +s /usr/bin/light
         mv $HOME/.config/i3/i3status-jrsbkp.conf $HOME/.config/i3/i3status.conf
         mv $HOME/.config/i3/config-jrsbkp.conf $HOME/.config/i3/config
-        criarArq 'light' "$HOME/.config/i3/brightness"
+        echo 'ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp wheel $sys$devpath/brightness", RUN+="/bin/chmod g+w $sys$devpath/brightness"' | sudo tee /etc/udev/rules.d/backlight.rules
+        #criarArq 'light' "$HOME/.config/i3/brightness"
         i3 restart
+        sleep 0
 }
 
