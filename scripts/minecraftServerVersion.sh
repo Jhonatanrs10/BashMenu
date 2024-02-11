b9d88bfabb619851164b9f99df4c28b89c567d34
Jhonatanrs10 <jhonatanrs10@hotmail.com>
2024-02-10 21:48:56 -0300

    att paper mine server

--- scripts/minecraftServerVersion.sh
+++ scripts/minecraftServerVersion.sh
@@ -3,14 +3,15 @@ installMinecraftServerVersion(){
 	echo "NOME DO SERVIDOR/PASTA/BIN (SERA USADO NOS 3)"
 	read nomeMSV
 
+	
 	yesorno "MINECRAFT SERVER MOHIST 1.7.10" "setLink https://mohistmc.com/builds/1.7.10/Mohist-1.7.10-46-server.jar"
 	yesorno "MINECRAFT SERVER MOHIST 1.16.5" "setLink https://mohistmc.com/builds/1.16.5/mohist-1.16.5-1107-server.jar"
-	
 	yesorno "MINECRAFT SERVER FORGE 1.7.10" "setLink https://maven.minecraftforge.net/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10/forge-1.7.10-10.13.4.1614-1.7.10-installer.jar"
 	yesorno "MINECRAFT SERVER FORGE 1.16.5" "setLink https://maven.minecraftforge.net/net/minecraftforge/forge/1.16.5-36.2.34/forge-1.16.5-36.2.34-installer.jar"
 	yesorno "MINECRAFT SERVER FORGE 1.18.2" "setLink https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.2.0/forge-1.18.2-40.2.0-installer.jar"
-	
 	yesorno "MINECRAFT SERVER LATEST RELEASE" "setLink https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar"
+	yesorno "MINECRAFT SERVER PAPER LATEST RELEASE" "setLink https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/422/downloads/paper-1.20.4-422.jar"
+
 
 	echo -e "[INFO] - CRIANDO DIRETORIOS... - [INFO]"
 	criaDiretorio "diretorioServer" "$dBashMenu/MinecraftServer-$nomeMSV"
