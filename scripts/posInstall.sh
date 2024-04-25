fdeaaa9167747956584358bef6ed339dd7b0a910
Jhonatanrs10 <jhonatanrs10@hotmail.com>
2024-04-25 13:59:25 -0300

    att

--- scripts/posInstall.sh
+++ scripts/posInstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 myBase="pulseaudio pulseaudio-bluetooth samba xarchiver papirus-icon-theme breeze-gtk xcursor-comix ntfs-3g dosfstools os-prober nano vim git neofetch gufw gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer ffmpeg fwupd samba gvfs-smb flatpak gvfs gvfs-mtp gvfs-smb udisks2 polkit polkit-gnome net-tools bluez bluez-tools bluez-utils joyutils man-db gnu-free-fonts noto-fonts noto-fonts-cjk noto-fonts-emoji cmatrix htop"
-myI3wm="i3 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings font-manager dmenu rofi i3lock i3status feh imagemagick nitrogen acpilight volumeicon pcmanfm scrot xsel terminology lxrandr lxappearance xfce4-taskmanager xfce4-power-manager xfce4-appfinder galculator system-config-printer blueman pavucontrol network-manager-applet wireless_tools xreader mpv"
+myI3wm="i3 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings font-manager dmenu rofi i3lock i3status feh imagemagick nitrogen acpilight volumeicon thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman scrot xsel terminology lxrandr lxappearance xfce4-taskmanager xfce4-power-manager xfce4-appfinder galculator system-config-printer blueman pavucontrol network-manager-applet wireless_tools xreader mpv"
 
 posInstallDeb(){
         yesorno "Deseja testar conexao?" "teste_internet"
@@ -202,7 +202,7 @@ set $textFont FreeMono 8
 #set $appMenu xfce4-appfinder
 set $appMenu rofi -combi-modi drun#ssh#combi -show combi -window-title Rofi -scroll-method 1 -show-icons -combi-display-format "{text} ({mode})" -config $HOME/.config/i3/rofi.rasi 
 set $appTerminal terminology
-set $appFiles pcmanfm
+set $appFiles thunar
 set $appBrowser chromium
 set $appF1 pavucontrol
 set $appF2 galculator
