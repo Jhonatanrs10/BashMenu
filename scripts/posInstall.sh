b732e06f47a8f442cc9bf466410a3ae358684b0d
Jhonatanrs10 <jhonatanrs10@hotmail.com>
2024-05-04 12:12:58 -0300

    att

--- scripts/posInstall.sh
+++ scripts/posInstall.sh
@@ -152,6 +152,7 @@ sudo chmod 777 /usr/share/backgrounds
 sudo tee /etc/lightdm/lightdm-gtk-greeter.conf <<< '[greeter]
 theme-name = Breeze-Dark
 icon-theme-name = Papirus-Dark
+cursor-theme-name = ComixCursors-Black
 indicators = ~session;~spacer;~clock;~spacer;~power
 background = /usr/share/backgrounds/main.png
 font-name = FreeMono 10'
