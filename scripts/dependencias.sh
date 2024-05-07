46af8395ac2c145def81cffa36fb1964c1287909
Jhonatanrs10 <jhonatanrs10@hotmail.com>
2024-05-07 13:43:42 -0300

    att

--- scripts/dependencias.sh
+++ scripts/dependencias.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-fullPackages="git imagemagick net-tools zip unzip rar unrar tar bzip2 bash bats curl jq nano wget"
+fullPackages="git imagemagick net-tools bzip2 cpio gzip lha xz lzop p7zip tar unace unrar zip unzip bash bats curl jq nano wget"
 javaPackages="openjdk-8-jdk openjdk-11-jdk openjdk-17-jdk jre8-openjdk-headless jre8-openjdk jdk8-openjdk openjdk8-doc openjdk8-src jre11-openjdk-headless jre11-openjdk jdk11-openjdk openjdk11-doc openjdk11-src jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src"
 dBashMenu="$HOME/.bashmenu"
 nomeRun="run"
